f1e36ac956976de9bb36dfafa810c2e8bf40d7d2
Franco Mangone <francomangone18@gmail.com>
2025-08-01 18:21:21 -0300

    adds new image to the side of reading lists in home

--- src/features/home/components/reading-lists.tsx
+++ src/features/home/components/reading-lists.tsx
@@ -1,6 +1,7 @@
 import { BookOpen } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 interface ReadingListsProps {
   lang: string;
@@ -13,8 +14,8 @@ export function ReadingLists({ lang, dictionary }: ReadingListsProps) {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://arkana.blog";
 
   return (
-    <section className="relative w-full py-8">
-      <div className="relative flex flex-row gap-8 container mx-auto px-4 py-8 md:px-6 lg:px-8 max-w-8xl z-10">
+    <section className="relative w-full py-8 mt-12">
+      <div className="relative flex flex-row gap-8 container mx-auto px-12 lg:py-8 md:px-6 lg:px-8 max-w-8xl z-10">
         <BookOpen className="ml-4 md:ml-0 shrink-0 w-12 h-12 text-primary-500" />
         <div className="md:max-w-120">
           <h1 className="text-4xl font-bold mb-2">
@@ -30,7 +31,21 @@ export function ReadingLists({ lang, dictionary }: ReadingListsProps) {
             {dictionary?.home?.readingLists?.viewAll}
           </Link>
         </div>
+        <div className="hidden lg:block lg:grow-1">
+          <Image
+            src="/reading-list-render.png"
+            alt="Reading list interface preview"
+            width={600}
+            height={400}
+            className="w-full h-full object-cover object-left"
+            priority
+          />
+        </div>
       </div>
+
+      {/* <div className="absolute top-0 bottom-0 pointer-events-none">
+        
+      </div> */}
     </section>
   );
 }
