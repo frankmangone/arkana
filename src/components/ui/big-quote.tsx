174a7fb9a96f2b8109fe948804f30479147fe3c3
Franco Mangone <francomangone18@gmail.com>
2026-01-20 15:29:18 -0300

    chore: modifies big quotes

--- src/components/ui/big-quote.tsx
+++ src/components/ui/big-quote.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export function BigQuote({ children }: { children: React.ReactNode }) {
     return (
-        <span className="block text-2xl leading-8 md:text-[1.75rem] md:leading-9 text-center my-14 mx-10 font-medium text-[#e9deff] break-words">
-            {children}
-        </span>
+        <div className="flex flex-col items-center my-8">
+            <div className="flex justify-between w-full opacity-70">
+                <Image src="/quote.png" alt="Big Quote" width={30} height={30} />
+                <Image src="/quote.png" alt="Big Quote" width={30} height={30} className="rotate-90" />
+            </div>
+            <span className="block text-2xl leading-8 md:text-[1.75rem] md:leading-9 text-center my-0 mx-[30px] font-medium text-[#e9deff] break-words">
+                {children}
+            </span>
+            <div className="flex justify-between w-full opacity-70">
+                <Image src="/quote.png" alt="Big Quote" width={30} height={30} className="rotate-270" />
+                <Image src="/quote.png" alt="Big Quote" width={30} height={30} className="rotate-180" />
+            </div>
+        </div>
     );
 };
