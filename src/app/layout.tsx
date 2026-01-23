4a1a51552fc341132539f00fe34b83ea19c45999
Franco Mangone <francomangone18@gmail.com>
2026-01-23 14:31:10 -0300

    fix: adjusts simple analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -240,8 +240,8 @@ export default async function RootLayout({
             <AuthProvider>{children}</AuthProvider>
           </Web3Provider>
         </ThemeProvider>
-        {process.env.NEXT_PUBLIC_DEV_MODE !== "true" && <Script async src="https://scripts.simpleanalyticscdn.com/latest.js" />}
       </body>
+      {process.env.NEXT_PUBLIC_DEV_MODE !== "true" && <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />}
     </html>
   );
 }
